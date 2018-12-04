0fe6f2f9ceb57821c6927c37afade5bcdf8b47b6
Harm de Raaff <harm.apple@gmail.com>
2018-12-04 10:40:50 +0100

    Renamed shell script and added comments in rule

--- tools/ExportDatasetErrorsAndWarnings.r
+++ tools/ExportDatasetErrorsAndWarnings.r
@@ -1,6 +1,15 @@
+# Functionality:
+# Find files within the dynamic area of an intake study that have errors and/or warnings at file level.
+# A check for errors/warnings is performed ONLY on file level. 
+# Errors that can be found on dataset-toplevel or on collection level within a dataset, are NOT reported
+
+# Parameters:
+# - Study: Name of the study the export has to search 
+
+# Run with DatasetErrorsAndWarnins.sh script to have the export added to a csv file.
+
 ExportDatasetErrorsAndWarnings {
    ## OVERRULE PARAMS FOR NOW as I wasn't able to add multiple input params 
-   *userNameParam="datamanager";
 #   *studyParam="test";
 
    # Possibly use uuClientFullName as user, or $userNameClienterNameClient;  ????????????????????????
@@ -8,7 +17,8 @@ ExportDatasetErrorsAndWarnings {
 
 
    # Initialisation of variables based on command line parameters
-   *user = *userNameParam;
+#   *user="datamanager";
+   *user = uuClientFullName
    *study = *studyParam;
    *datamanagerGroup = 'grp-datamanager-' ++ *study;
    *studyFolder = "/" ++ $rodsZoneClient ++ "/"  ++ 'home/grp-intake-' ++ *studyParam;
